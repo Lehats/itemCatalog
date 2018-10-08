cdcd3ad8ee6c55e21c71b5fc00bda999baabac24
lehats <fst@minus-plus.net>
2018-10-08 15:44:48 +0200

    design html and css basic layout

--- catalog.py
+++ catalog.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+@app.route('/')
+def getMainPage():
+    return render_template('main.html')
 
 if (__name__ == '__main__'):
     app.debug = True
