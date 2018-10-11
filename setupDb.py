876c59f0ea9eaff8a1552e6155cd75d93569eb8a
lehats <fst@minus-plus.net>
2018-10-11 15:16:08 +0200

    add table Users to db and create password - hash and -verify methods

--- setupDb.py
+++ setupDb.py
@@ -2,11 +2,26 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from passlib.apps import custom_app_context as pwd_context
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
  
 Base = declarative_base()
- 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    username = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    password_hash = Column(String(64))
+
+    def hashThePassword(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
 class Categories(Base):
     __tablename__ = 'categories'
 
